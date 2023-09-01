d8e7cfaa205fdcd310db8d8cea862c4ca4faef16
Vinicius Peretta <viniassisperetta@gmail.com>
2023-09-01 14:31:40 -0300

    Signed-off-by: Vinicius Peretta <viniassisperetta@gmail.com>

--- Lista 06 CodingBat.py
+++ Lista 06 CodingBat.py
@@ -103,7 +103,12 @@ def apaga(s, n):
 # troca('a') -> 'a'
 # troca('ab') -> 'ba'
 def troca(s):
-  
+  v = []
+  if len(s) <= 1: 
+    return s
+  else:
+    return s[-1] + s[1:-1] + s[0]
+
 
 # Provided simple test() function used in main() to print
 # what each function returns vs. what it's supposed to return.
